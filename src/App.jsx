a5a0db48065e658e848fd6cc9f5544acd12296ab
Tom Oborne <tpoborne@gmail.com>
2024-07-10 16:02:44 +0100

    Removed footer import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header';
 import View from './components/View';
-import Footer from './components/Footer';
 import { WebSocketProvider } from './contexts/WebSocketContext';
 
 const App = () => {
