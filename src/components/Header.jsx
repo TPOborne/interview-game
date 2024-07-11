0947817d5fb52930c8b58aa097b9e6548d340fc1
Tom Oborne <tpoborne@gmail.com>
2024-07-11 23:03:46 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import LanguageIcon from '../assets/icons/language.svg?react';
 const Header = () => {
   return (
     <header>
-      <h3>Word Race</h3>
-      <div className="iconWrapper small">
+      Word Race
+      {/* <div className="iconWrapper small">
         <LanguageIcon />
-      </div>
+      </div> */}
     </header>
   )
 };
